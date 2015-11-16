9676d8792522cbb4012cb38d9d3ac12730929f59
wad <suguby@gmail.com>
2015-11-17 00:53:00 +0300

    затравка вывода списка скринкастов

--- screencasts/views.py
+++ screencasts/views.py
@@ -2,6 +2,15 @@
 
 from django.views.generic import TemplateView
 
+from screencasts.models import Screencast
+
 
 class ScreencastsListView(TemplateView):
     template_name = 'screencasts/index.html'
+
+    def get_context_data(self, **kwargs):
+        screencasts = Screencast.objects.all()
+        context = dict(
+            screencasts=screencasts
+        )
+        return context
