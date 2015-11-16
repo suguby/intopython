ece21bfe4ed3131ff79609f403323cd1079c260e
wad <suguby@gmail.com>
2015-11-17 00:03:39 +0300

    добавление скринкастов в админку

--- screencasts/admin.py
+++ screencasts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from screencasts.models import ScreencastSection, Screencast
+
+admin.site.register(ScreencastSection)
+admin.site.register(Screencast)
