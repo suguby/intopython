32eb711d40de08fd00869879fa19f2a7cd51caac
mitri4 <mitri4@bk.ru>
2015-11-21 03:01:14 +0300

    create bodel Forum

--- forum/models.py
+++ forum/models.py
@@ -10,3 +10,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Forum(models.Model):
+    category = models.ForeignKey(Category, related_name='forums')
+    name = models.CharField(_("Name"), max_length=255)
+    position = models.IntegerField(_("Position"), default=0)
+    description = models.TextField(_("Description"), blank=True)
+    is_closed = models.BooleanField(_("Is closed"), default=False)
+
+    class Meta:
+        ordering = ['position']
+
+    def __str__(self):
+        return self.name
