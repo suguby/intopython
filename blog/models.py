1cf396981ed93bb6785f2c799d80a1e9482ea0a1
wad <intopython@gmail.com>
2015-12-05 16:11:22 +0300

    + blog record model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class BlogRecord(models.Model):
+    title = models.CharField(max_length=64)
+    message = models.TextField()
+
+    def __str__(self):
+        return 'BlogRecord: {}'.format(self.title)
