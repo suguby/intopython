3e2c2a95cf7ddb86d375b27ea0c78f3575b9b92c
wad <intopython@gmail.com>
2015-12-05 16:21:41 +0300

    + created_at field

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class BlogRecord(models.Model):
     title = models.CharField(max_length=64)
     message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    modified_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return 'BlogRecord: {}'.format(self.title)
