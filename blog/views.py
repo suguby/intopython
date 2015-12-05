12bd21078c88ae5416dcf569c871f84e3408809f
wad <intopython@gmail.com>
2015-12-05 16:15:55 +0300

    + blog record model in view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from blog.models import BlogRecord
+
 
 class BlogView(TemplateView):
     template_name = 'blog/index.html'
+
+    def get_context_data(self, **kwargs):
+        records = BlogRecord.objects.all()
+        context = dict(records=records)
+        return context
