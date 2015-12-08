add24adaac71adac9e72aaeb75a8d5154ebeed3f
wad <suguby@gmail.com>
2015-12-08 17:57:03 +0300

    прикрутил модальный диалог для регистрации + jquery в статик

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
 
 class LandingView(TemplateView):
     template_name = 'landing/index.html'
+
+
+class LandingRegisterView(View):
+
+    def post(self):
+        email = self.request.POST.get('email')
+
 
