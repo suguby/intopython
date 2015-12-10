42de95e391b0834d7277132df2904d5e12fccc5f
wad <suguby@gmail.com>
2015-12-10 15:49:11 +0300

    модель зарегестрировавшегося через лендинг пользователя

--- landing/models.py
+++ landing/models.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from model_utils import Choices
 
-# Create your models here.
+
+class LendingRegistration(models.Model):
+    STATUSES = Choices(('new', 'Новый'), ('accepted', 'Оплативший'),  ('rejected', 'Отказник'), )
+
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=64)
+    phone = models.CharField(max_length=32, null=True, blank=True)
+    status = models.CharField(max_length=16, choices=STATUSES, default=STATUSES.new)
+    register_at = models.DateTimeField(auto_now_add=True)
