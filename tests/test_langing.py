e3131ed4bb94edf0f8cca171149d4ddeb6b7283a
wad <suguby@gmail.com>
2016-01-21 23:54:04 +0300

    три теста на регистрацию лендинга

--- tests/test_langing.py
+++ tests/test_langing.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+
+from landing.models import LendingRegistration
 
 
 class TestLanding(TestCase):
 
     def test_submit(self):
-        self.client
+        data = dict(email='test@test.ru', name='тест', phone='12345')
+        self.client.post(reverse('landing'), data=data)
+        user = LendingRegistration.objects.get(email=data['email'])
+        self.assertEquals(user.name, data['name'])
+
+    def test_invalid_submit(self):
+        data = dict(email='test@test', name='тест', phone='12345')
+        self.client.post(reverse('landing'), data=data)
+        queryset = LendingRegistration.objects.filter(email=data['email'])
+        self.assertEquals(len(queryset), 0)
+
+    def test_empty_submit(self):
+        self.client.post(reverse('landing'), data=dict())
+        queryset = LendingRegistration.objects.all()
+        self.assertEquals(len(queryset), 0)
