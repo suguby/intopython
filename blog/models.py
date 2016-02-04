b7501b722f1843f38177fb8f18004f41f8a65aa5
wad <suguby@gmail.com>
2016-02-04 22:26:51 +0300

    вывод списка записей блога

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,16 @@
+from django.db import models
+
 from articles.models import Article, ArticleByTypeManager
 
 
 class Blog(Article):
+    tag = models.CharField(max_length=100, verbose_name='Таги', null=True, blank=True)
 
     class Meta:
-        proxy = True
+        db_table = 'blogs'
 
-    objects = ArticleByTypeManager(type=Article.TYPES.blog)
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.type = Article.TYPES.blog
+        super(Blog, self).save(
+            force_insert=force_insert, force_update=force_update,
+            using=using, update_fields=update_fields)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,35 @@
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import TemplateView
+
+from blog.models import Blog
 
 
 class BlogView(TemplateView):
     template_name = 'blog/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data( **kwargs)
+        sc_queryset = Blog.objects.filter(
+            status=Blog.STATUSES.publ,
+        ).order_by('-created_at')
+        section_filter = self.request.GET.get('section')
+        query_string = ''
+        if section_filter:
+            sc_queryset = sc_queryset.filter(section__slug=section_filter)
+            query_string += 'section={}&'.format(section_filter)
+        paginator = Paginator(sc_queryset, 2)
+        try:
+            articles = paginator.page(number=self.request.GET.get('page'))
+        except PageNotAnInteger:
+            articles = paginator.page(1)
+        except EmptyPage:
+            articles = paginator.page(paginator.num_pages)
+        context.update(
+            articles=articles,
+            query_string=query_string,
+        )
+        return context
+
 
 
 class BlogDetailView(TemplateView):
