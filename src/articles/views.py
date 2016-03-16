f2e89848c7a45bc75d893e6cbfdac603093a9df1
wad <suguby@gmail.com>
2016-03-16 23:40:25 +0300

    багфикс облака тегов

--- src/articles/views.py
+++ src/articles/views.py
@@ -16,22 +16,7 @@ class ArticlesBaseView(BaseTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        tag = self.request.GET.get('tag')
-        if not self.articles_url_filter and tag:
-            self.articles_filter = Q(tags__slug=tag)
-            self.articles_url_filter = 'tag={}&'.format(tag)
-        self.articles_filter &= Q(status=self.model.STATUSES.publ)
-        articles = self.model.objects.filter(
-            self.articles_filter
-        ).order_by('-created_at').prefetch_related('tagged_items__tag')
-        if self.PAGE_SIZE:
-            paginator = Paginator(articles, self.PAGE_SIZE)
-            try:
-                articles = paginator.page(number=self.request.GET.get('page'))
-            except PageNotAnInteger:
-                articles = paginator.page(1)
-            except EmptyPage:
-                articles = paginator.page(paginator.num_pages)
+        articles = self.model.objects.filter(status=self.model.STATUSES.publ)
 
         # TODO уменьшить количество запросов
         slugs_counter = Counter((slug for sc in articles for slug in sc.tags.slugs()))
@@ -41,6 +26,24 @@ class ArticlesBaseView(BaseTemplateView):
             if tag.slug in slugs_counter:
                 tag.range = int(5 - slugs_counter[tag.slug] // count_range)
                 tags.append(tag)
+
+        tag = self.request.GET.get('tag')
+        if not self.articles_url_filter and tag:
+            self.articles_filter = Q(tags__slug=tag)
+            self.articles_url_filter = 'tag={}&'.format(tag)
+        articles = articles.filter(
+            self.articles_filter
+        ).order_by('-created_at').prefetch_related('tagged_items__tag')
+
+        if self.PAGE_SIZE:
+            paginator = Paginator(articles, self.PAGE_SIZE)
+            try:
+                articles = paginator.page(number=self.request.GET.get('page'))
+            except PageNotAnInteger:
+                articles = paginator.page(1)
+            except EmptyPage:
+                articles = paginator.page(paginator.num_pages)
+
         context.update(
             articles=articles,
             tags=tags,
