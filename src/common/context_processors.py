4cccff6bbb6c735f93529fc3c912c2d8c9aeca5a
wad <suguby@gmail.com>
2016-03-18 13:55:02 +0300

    убрал user из контектс процессора - он доступен в шаблонах через request.user

--- src/common/context_processors.py
+++ src/common/context_processors.py
@@ -3,7 +3,6 @@
 
 def common_variables_to_context(request):
     return dict(
-        user=request.user,
         bltn=__builtins__,
     )
 
--- src/common/css_filters.py
+++ src/common/css_filters.py
@@ -3,6 +3,8 @@ import re
 
 
 def css_class(elem, klass):
+    # TODO проверять есть ли уже class= и добавлять, может параметр сделать - заменять/добавлять
+    # TODO и вообще сделать класс, там предкомпилить реджексы
     elem = str(elem)
     matched = re.search(r'^(\s*<\w+)\s*', elem)
     if not matched:
