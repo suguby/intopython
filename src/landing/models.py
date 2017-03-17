b0527f074eb31342989a8df292f96f859de4a4d7
wad <suguby@gmail.com>
2017-03-17 15:46:04 +0300

    форму регистрации на модел форм, подсушил шаблон регистраци, добавил поле детайлс

--- src/landing/models.py
+++ src/landing/models.py
@@ -9,10 +9,13 @@ from src.common.models import AbstractModel
 class LendingRegistration(AbstractModel):
     STATUSES = Choices(('new', 'Новый'), ('accepted', 'Оплативший'),  ('rejected', 'Отказник'), )
 
-    name = models.CharField(max_length=128)
-    email = models.EmailField(max_length=64)
-    phone = models.CharField(max_length=32, null=True, blank=True)
-    status = models.CharField(max_length=16, choices=STATUSES, default=STATUSES.new)
+    name = models.CharField(verbose_name='Ваше имя', max_length=128)
+    email = models.EmailField(verbose_name='Ваш email', max_length=64)
+    phone = models.CharField(verbose_name='Ваш номер телефона или skype-логин (необязательно)',
+                             max_length=32, null=True, blank=True)
+    details = models.TextField(verbose_name='Расскажите о себе или задайте вопросы (необязательно)',
+                               max_length=1024, null=True, blank=True)
+    status = models.CharField(verbose_name='Статус', max_length=16, choices=STATUSES, default=STATUSES.new)
     registered_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
