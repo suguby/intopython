a75ce66284bb6a7af87d26c7e1d6e33689797bb8
wad <suguby@gmail.com>
2017-03-17 15:05:08 +0300

    + отсылка почты при регистрации на лендинг

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.core import mail
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
@@ -35,6 +37,18 @@ class CoursesSignView(BaseTemplateView):
             data = form.cleaned_data
             LendingRegistration.objects.get_or_create(
                 email=data['email'], defaults=dict(name=data['name'], phone=data['phone']))
+            message = (
+                'Someone registered on lending: ',
+                'email: '.format(data['email']),
+                'name: '.format(data['name']),
+                'phone: '.format(data['phone']),
+            )
+            mail.send_mail(
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=['itogo@mail.ru', ],
+                subject='Lending registration',
+                message='\n'.join(message),
+            )
             return HttpResponseRedirect(reverse('courses_signed'))
         return self.render_to_response(context=context)
 
