41bf7a8236c234fd5386c80bc478c76f1014da67
wad <suguby@gmail.com>
2017-08-11 16:10:59 +0300

    added OAuth with VK Google+ (github doesn't return email in some cases)

--- intopython/settings/oauth.py
+++ intopython/settings/oauth.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from social_core.backends.facebook import FacebookOAuth2
+# import social_core.backends.google
+
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.vk.VKOAuth2',
+    # 'social_core.backends.github.GithubOAuth2',  # TODO обработка нет емейла от сервиса
+    'social_core.backends.google.GoogleOAuth2',
+
     # 'social_core.backends.open_id.OpenIdAuth',
-    # 'social_core.backends.google.GoogleOAuth',
-    # 'social_core.backends.vkontakte.',
-    # 'social_core.backends.facebook.FacebookBackend',
-    # 'social_core.backends.google.GoogleOAuth2Backend',
     # 'social_core.backends.twitter.TwitterBackend',
     # 'social_core.backends.yandex.YandexOAuth2Backend',
     # 'social_core.backends.mailru.MailruBackend',
@@ -16,15 +17,30 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_SCOPE = ['email']
+SOCIAL_AUTH_USER_FIELDS = ['email']
+
+
 # http://python-social-auth.readthedocs.io/en/latest/backends/facebook.html
 SOCIAL_AUTH_FACEBOOK_KEY = '...'
 SOCIAL_AUTH_FACEBOOK_SECRET = '...'
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-SOCIAL_AUTH_FACEBOOK_USER_FIELDS = ['email']
 # SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
 #     'locale': 'ru_RU',
 #     'fields': 'email'
 # }
+
+
+# http://python-social-auth.readthedocs.io/en/latest/backends/vk.html
+SOCIAL_AUTH_VK_OAUTH2_KEY = ''
+SOCIAL_AUTH_VK_OAUTH2_SECRET = ''
+
+# http://python-social-auth.readthedocs.io/en/latest/backends/github.html
+SOCIAL_AUTH_GITHUB_KEY = '...'
+SOCIAL_AUTH_GITHUB_SECRET = '...'
+# Для проды обновить Authorization callback URL !!!
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '...'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '...'
 
 try:
     from .local_oauth import *
